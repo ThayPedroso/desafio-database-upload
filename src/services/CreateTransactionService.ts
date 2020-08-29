46a76178f434b610726187316b8da3667f697dba
ThayPedroso <thayla.pedroso88@gmail.com>
2020-08-29 00:03:39 -0300

    createTransactionRoute

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+    let category_id;
+
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: { category },
+    });
+
+    if (!checkCategoryExists) {
+      const newCategory = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(newCategory);
+
+      category_id = newCategory.id;
+    } else {
+      category_id = checkCategoryExists.id;
+    }
+
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
