589803b400fc1fca8628ac7e1433c80d91a50629
ThayPedroso <thayla.pedroso88@gmail.com>
2020-09-01 22:13:51 -0300

    addedCors

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -23,8 +23,11 @@ class Transaction {
   @Column()
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
   @CreateDateColumn()
